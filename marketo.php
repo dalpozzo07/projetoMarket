3fc6ffc1277fbae4bdc2f5cc2b687cdff2bc3499
Eduardo <dalpozzo07@gmail.com>
2025-05-21 09:44:22 -0300

    Função de login

--- marketo.php
+++ marketo.php
@@ -1,1 +1,10 @@
 <?php
+
+function login($usuario, $senha){
+   if($usuario == "admin" && $senha == "1404"){
+    return true;
+   }else{
+    echo "O usuário ou senha estão incorretos" . PHP_EOL;
+    return false;
+   }
+}
