cd27285851d500ab95d1cc2d94aebe6b46cb2da8
Eduardo <dalpozzo07@gmail.com>
2025-05-21 11:53:40 -0300

    arrumando função de login, e incrementando função de cadastrar

--- marketo.php
+++ marketo.php
@@ -1,10 +1,26 @@
 <?php
 
-function login($usuario, $senha){
-   if($usuario == "admin" && $senha == "1404"){
-    return true;
-   }else{
-    echo "O usuário ou senha estão incorretos" . PHP_EOL;
-    return false;
-   }
+$usuarios = [
+    'admin' => '1404'
+];
+
+function login($usuario, $senha, $usuarios){
+    if (isset($usuarios[$usuario]) && $usuarios[$usuario] === $senha) {
+        return true;
+    } else {
+        echo "O usuário ou senha estão incorretos" . PHP_EOL;
+        return false;
+    }
+}
+
+function cadastrarUsuario(&$usuarios){
+    $usuario = readline("Digite o novo usuário: ");
+    $senha = readline("Digite uma senha para o novo usuário: ");
+
+    if (isset($usuarios[$usuario])) {
+        echo "Usuário já existe." . PHP_EOL;
+    } else {
+        $usuarios[$usuario] = $senha;
+        echo "Usuário cadastrado com sucesso!" . PHP_EOL;
+    }
 }
